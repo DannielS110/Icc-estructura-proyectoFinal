a42b1d0ea394f31019a1c41f60f7e68512c10b54
NelsonV362 <nvillalta54@gmail.com>
2025-07-27 12:40:38 -0500

    agregacion clase SolverType

--- src/model/SolverType.java
+++ src/model/SolverType.java
@@ -1,4 +1,34 @@
 package model;
 
-public class SolverType {
+public enum SolverType {
+    BFS("BFS"),
+    DFS("DFS"),
+    RECURSIVO("Recursivo (2 dir)"),
+    RECURSIVO_COMPLETO("Recursivo (4 dir)"),
+    RECURSIVO_BT("Recursivo (4 dir + BT)");
+
+    private final String name;
+
+    SolverType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the enum value corresponding to the input string (if it exists).
+     *
+     * @param name A string
+     * @return The SolverType with the name corresponding to the input string (if it exists).
+     */
+    public static SolverType fromString(String name) {
+        for (SolverType solverType : SolverType.values()) {
+            if (solverType.name.equalsIgnoreCase(name)) {
+                return solverType;
+            }
+        }
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
